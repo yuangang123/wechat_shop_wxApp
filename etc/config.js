b017a91e190389d8c997d50fd0a44cc025a9ca28
yuangang <975131172@qq.com>
2017-11-15 18:24:42 +0800

    微信电商小程序前端微信小程序

---
+++ etc/config.js
@@ -0,0 +1,8 @@
+export default {
+	// 线上环境
+	// basePath: 'https://www.skyvow.cn/api', 
+	// domain: 'https://www.skyvow.cn/', 
+	// 本地环境
+  basePath: 'http://192.168.226.137:3000/api', 
+  domain: 'http://192.168.226.137:3000', 
+}
